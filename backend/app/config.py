1d149ae83734b82ee145dd70defb609ac1d71c52
Артем <buggy_art@mail.ru>
2026-03-25 13:03:37 +0300

    Увеличено время действия токена + некоторые незначительные изменения

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY", "secret-key")
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 100
     DATABASE_URL: str = "sqlite:///./currency_converter.db"
     EXCHANGE_API_KEY: str = os.getenv("e0f98be44a16fb2b0f1a4ecc")
     EXCHANGE_API_URL: str = "https://api.exchangerate-api.com/v4/latest/"
